7709894dee8e7d6b97b0531c4eb8ea3f70881d8b
nootanghimire <nootan.ghimire@gmail.com>
2015-11-28 02:38:31 +0545

    Allowed CORS on server, Cleaned up code and added some comments.

--- server/translate.php
+++ server/translate.php
@@ -1,7 +1,32 @@
 <?php
-	$url="https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ne&dt=t&q=".urlencode($_GET['string']);
-	$a= file_get_contents($url);
-	$b=explode(",",$a);
-	$c=explode('"', $b[0]);
-	echo $c[1];
+
+  /* Enable CORS */
+  header("Access-Control-Allow-Origin: *");
+
+  /* Require Config */
+  require_once 'config.php';
+
+  /* GET param */
+  $str = $_GET['string'];
+
+  /* urlencode it */
+  $str = urlencode($str);
+
+  /* URL */
+  $url='https://translate.googleapis.com/translate_a/single?client=gtx&sl='.
+    $source_lang.'&tl='.$target_lang.'&dt=t&q='.$str;
+
+
+  /* Get the Contents */
+  $contents = file_get_contents($url);
+
+  /* The variable $contents is in a format [[["<translatedText",,]]]
+   * , which cannot be parsed with json_decode. So we need a dirty hack
+   * that would enable us to grab that translatedtext.
+   *
+   * If someone has a better and optimized version, please send a
+   * pull request!
+   *
+   */
+  echo substr($contents, 4, strpos(substr($contents, 4), '"'));
 ?>
