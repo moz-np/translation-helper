27206fcaaa3b388957fcc451937adae6e73de767
Nishchal Gautam <gautam.nishchal@gmail.com>
2015-11-28 11:27:22 +0530

    it works

--- client/translate.js
+++ client/translate.js
@@ -4,7 +4,7 @@ Dirty hack
 textarea_id="id_target_f_0";
 url="http://hikathon/translate.php?string=";
 text_from=".translation-text.js-translation-text";
-document.onmouseover=function  (element) {
+document.onclick=function  (element) {
 	if(element.srcElement.id==textarea_id){
 		console.log(element)
 		text=document.querySelector(text_from).innerText;
@@ -12,11 +12,13 @@ document.onmouseover=function  (element) {
 		xhttp.onreadystatechange = function() {
 			if (xhttp.readyState == 4 && xhttp.status == 200) {
 				console.log(xhttp.responseText);
-				document.querySelector("#"+textarea_id).value=xhttp.responseText;
+				if(document.querySelector("#"+textarea_id).value==""){
+					document.querySelector("#"+textarea_id).value=xhttp.responseText;
+				}
 			}
 		};
 		xhttp.open("GET", url+text, true);
 		xhttp.send();
 	}
-	return true;
+	return false;
 }
