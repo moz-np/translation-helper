8de859f2fbe277c23669b8a92acea9089a46f8ec
nootanghimire <nootan.ghimire@gmail.com>
2015-12-03 19:20:30 +0545

    Standarized Code, Now works on Firefox

--- client/translate.js
+++ client/translate.js
@@ -1,15 +1,20 @@
 /**
-Dirty hack
+Dirty hack (for now), iterating
  */
 
 var textarea_id="id_target_f_0"; //current_textarea_target
 var url="<your_server_url>?string=";
 var text_from=".translation-text.js-translation-text";
 
-document.onclick = function  (element) {
-	var target = element.target || element.srcElement ;
-	if (target.id == textarea_id) {
-		var text = document.querySelector(text_from).innerText;
+document.onclick = function  (e) {
+
+	var elem, evt = e ? e:event;
+
+	if (evt.srcElement)  elem = evt.srcElement;
+	else if (evt.target) elem = evt.target;
+
+	if (elem.getAttribute('id') == textarea_id) {
+		var text = document.querySelector(text_from).textContent;
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -19,6 +24,7 @@ document.onclick = function  (element) {
 			}
 		};
 		xhttp.open("GET", url+text, true);
+		console.log(url+text);
 		xhttp.send();
 	}
 	return false;
